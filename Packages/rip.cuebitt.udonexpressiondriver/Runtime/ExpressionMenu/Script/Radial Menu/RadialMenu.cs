f469d2ee51f0775f1666383e27b3b06d66fc974f
cuebitt <136763482+cuebitt@users.noreply.github.com>
2025-11-03 13:16:18 -0800

    Bake border into wedge texture
    Less flexible, but much more simple

--- Packages/rip.cuebitt.udonexpressiondriver/Runtime/ExpressionMenu/Script/Radial Menu/RadialMenu.cs
+++ Packages/rip.cuebitt.udonexpressiondriver/Runtime/ExpressionMenu/Script/Radial Menu/RadialMenu.cs
@@ -14,14 +14,11 @@ namespace UdonExpressionDriver
         [Range(1, 8)] public int segmentCount = 8;
         public float innerRadius = 0.1f;
         public float outerRadius = 0.3f;
-        public float outlineThickness = 0.01f;
         public int radialSteps = 48;
-        public float outlineOffsetY = 0.0005f;
 
         [Header("Internal")]
         public GameObject[] segments = new GameObject[8];
         public Material gradientMaterial;
-        public Material outlineMaterial;
 
         private void Start()
         {
@@ -52,21 +49,17 @@ namespace UdonExpressionDriver
 
                 var meshHolder = seg.transform.Find("Mesh Holder");
                 if (meshHolder)
-                    meshHolder.localRotation = Quaternion.Euler(0f, (angleStep * i) - startAngle, 0f); // clockwise
+                    meshHolder.localRotation = Quaternion.Euler(0f, angleStep * i - startAngle, 0f); // clockwise
 
                 var mf = meshHolder ? meshHolder.GetComponent<MeshFilter>() : null;
-                if (mf)
-                    mf.sharedMesh = CreateWedgeMesh(angleStep, innerRadius, outerRadius, radialSteps, outlineThickness,
-                        outlineOffsetY);
+                if (mf != null)
+                    mf.sharedMesh = CreateWedgeMesh(angleStep, innerRadius, outerRadius, radialSteps);
 
                 var mr = meshHolder ? meshHolder.GetComponent<MeshRenderer>() : null;
-                if (mr) mr.sharedMaterials = new[] { gradientMaterial, outlineMaterial };
+                if (mr != null) mr.sharedMaterials = new[] { gradientMaterial };
 
                 var mc = meshHolder ? meshHolder.GetComponent<MeshCollider>() : null;
-                if (mc && mf)
-                {
-                    mc.sharedMesh = mf.sharedMesh;
-                }
+                if (mc != null && mf != null) mc.sharedMesh = mf.sharedMesh;
             }
         }
 
@@ -79,152 +72,66 @@ namespace UdonExpressionDriver
         /// <param name="innerR">Inner radius of the wedge.</param>
         /// <param name="outerR">Outer radius of the wedge.</param>
         /// <param name="steps">Number of subdivisions along the arc.</param>
-        /// <param name="outlineT">Thickness of the outline quads.</param>
-        /// <param name="offsetY">Vertical offset to prevent z-fighting.</param>
         /// <returns>A Mesh object representing the wedge with outline.</returns>
-        private static Mesh CreateWedgeMesh(float angleDeg, float innerR, float outerR, int steps, float outlineT,
-            float offsetY)
+        private static Mesh CreateWedgeMesh(float angleDeg, float innerR, float outerR, int steps)
         {
             var mesh = new Mesh();
 
-            // Precompute full circle rings
-            var innerRing = new Vector3[steps + 1];
-            var outerRing = new Vector3[steps + 1];
-            for (var i = 0; i <= steps; i++)
-            {
-                var a = Mathf.Deg2Rad * 360f / steps * i;
-                var dir = new Vector3(Mathf.Sin(a), 0f, Mathf.Cos(a));
-                innerRing[i] = dir * innerR;
-                outerRing[i] = dir * outerR;
-            }
+            // Determine the number of radial steps for this wedge
+            var endIdx = steps;
 
-            // Determine which portion of the rings belongs to this wedge
-            var endIdx = Mathf.RoundToInt(steps * angleDeg / 360f);
-
-            // Gradient vertices
-            var vertCount = (endIdx + 1) * 2;
-            var verts = new Vector3[vertCount + endIdx * 8 + 4]; // +outline
+            // Vertex arrays
+            // Each step has two vertices: inner and outer
+            var verts = new Vector3[(endIdx + 1) * 2];
             var uvs = new Vector2[verts.Length];
 
+            // Build vertices and UVs
             for (var i = 0; i <= endIdx; i++)
             {
-                var vi = i - 0;
-                verts[vi] = innerRing[i];
-                verts[vi + (endIdx - 0) + 1] = outerRing[i];
-                uvs[vi] = new Vector2(0.5f, 0f);
-                uvs[vi + (endIdx - 0) + 1] = new Vector2(0.5f, 1f);
+                var t = (float)i / endIdx; // normalized along the arc [0,1]
+                var angle = Mathf.Deg2Rad * angleDeg * t;
+
+                // Unit direction in XZ plane
+                var dir = new Vector3(Mathf.Sin(angle), 0f, Mathf.Cos(angle));
+
+                // Vertex positions
+                verts[i] = dir * innerR; // inner ring
+                verts[i + endIdx + 1] = dir * outerR; // outer ring
+
+                // UVs
+                // Map inner radius to v=0, outer radius to v=1
+                // Map angular position to u = 0..1 across the wedge
+                uvs[i] = new Vector2(t, 0f);
+                uvs[i + endIdx + 1] = new Vector2(t, 1f);
             }
 
-            // Gradient triangles
-            var trisGradient = new int[endIdx * 6];
-            for (int i = 0, t = 0; i < endIdx - 0; i++)
+            // Triangles
+            var tris = new int[endIdx * 6];
+            for (int i = 0, t = 0; i < endIdx; i++)
             {
-                var i1 = i + 1;
-                var i2 = i + endIdx + 1;
-                var i3 = i + endIdx + 2;
-                trisGradient[t++] = i;
-                trisGradient[t++] = i2;
-                trisGradient[t++] = i1;
-                trisGradient[t++] = i1;
-                trisGradient[t++] = i2;
-                trisGradient[t++] = i3;
+                var iInner1 = i + 1;
+                var iOuter0 = i + endIdx + 1;
+                var iOuter1 = i + endIdx + 2;
+
+                // Triangle 1
+                tris[t++] = i;
+                tris[t++] = iOuter0;
+                tris[t++] = iInner1;
+
+                // Triangle 2
+                tris[t++] = iInner1;
+                tris[t++] = iOuter0;
+                tris[t++] = iOuter1;
             }
-
-            // Outline vertices and triangles
-            int v = vertCount, tIdx = 0;
-
-            var trisOutline = new int[endIdx * 12 + 6];
-
-            for (var i = 0; i < endIdx; i++)
-            {
-                var d0 = innerRing[i];
-                var d1 = innerRing[i + 1];
-                var edgeDir = (outerRing[i + 1] - outerRing[i]).normalized;
-                var radialOffset = Vector3.Cross(Vector3.up, edgeDir).normalized * outlineT;
-
-                // Inner rim quad (outer->inner)
-                AddQuad(verts, trisOutline, v, d0, d1, d0 - radialOffset, d1 - radialOffset, offsetY, tIdx);
-                v += 4;
-                tIdx += 6;
-
-                // Outer rim quad
-                var od0 = outerRing[i];
-                var od1 = outerRing[i + 1];
-                AddQuad(verts, trisOutline, v, od0 + radialOffset, od1 + radialOffset, od0, od1, offsetY, tIdx);
-                v += 4;
-                tIdx += 6;
-            }
-
-            // Radial start edge
-            var startDir = innerRing[0].normalized;
-            var startOffset = Vector3.Cross(Vector3.up, startDir).normalized * outlineT;
-            AddQuadFlipped(verts, trisOutline, v, innerRing[0], outerRing[0],
-                innerRing[0] + startOffset, outerRing[0] + startOffset, offsetY, tIdx);
 
             mesh.vertices = verts;
             mesh.uv = uvs;
-            mesh.subMeshCount = 2;
-            mesh.SetTriangles(trisGradient, 0);
-            mesh.SetTriangles(trisOutline, 1);
+            mesh.triangles = tris;
+
             mesh.RecalculateNormals();
             mesh.RecalculateBounds();
+
             return mesh;
-        }
-
-
-        /// <summary>
-        /// Adds a quad to the vertex and triangle arrays with standard counter-clockwise winding.
-        /// </summary>
-        /// <param name="v">Array of vertices to write into.</param>
-        /// <param name="t">Array of triangle indices to write into.</param>
-        /// <param name="s">Starting index in the vertex array for this quad.</param>
-        /// <param name="v0">Top-left vertex of the quad (outer corner).</param>
-        /// <param name="v1">Top-right vertex of the quad (outer corner).</param>
-        /// <param name="v2">Bottom-left vertex of the quad (inner corner).</param>
-        /// <param name="v3">Bottom-right vertex of the quad (inner corner).</param>
-        /// <param name="y">Vertical offset to apply to all vertices (usually small to prevent z-fighting).</param>
-        /// <param name="ti">Starting index in the triangle array for this quad.</param>
-        private static void AddQuad(Vector3[] v, int[] t, int s, Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3,
-            float y, int ti)
-        {
-            v[s] = v0 + Vector3.up * y;
-            v[s + 1] = v1 + Vector3.up * y;
-            v[s + 2] = v2 + Vector3.up * y;
-            v[s + 3] = v3 + Vector3.up * y;
-            t[ti] = s;
-            t[ti + 1] = s + 2;
-            t[ti + 2] = s + 1;
-            t[ti + 3] = s + 1;
-            t[ti + 4] = s + 2;
-            t[ti + 5] = s + 3;
-        }
-
-        /// <summary>
-        /// Adds a quad to the vertex and triangle arrays with flipped clockwise winding.
-        /// Useful for edges that need reversed normals (e.g., radial start edges).
-        /// </summary>
-        /// <param name="v">Array of vertices to write into.</param>
-        /// <param name="t">Array of triangle indices to write into.</param>
-        /// <param name="s">Starting index in the vertex array for this quad.</param>
-        /// <param name="v0">Top-left vertex of the quad.</param>
-        /// <param name="v1">Top-right vertex of the quad.</param>
-        /// <param name="v2">Bottom-left vertex of the quad.</param>
-        /// <param name="v3">Bottom-right vertex of the quad.</param>
-        /// <param name="y">Vertical offset applied to all vertices.</param>
-        /// <param name="ti">Starting index in the triangle array for this quad.</param>
-        private static void AddQuadFlipped(Vector3[] v, int[] t, int s, Vector3 v0, Vector3 v1, Vector3 v2, Vector3 v3,
-            float y, int ti)
-        {
-            v[s] = v0 + Vector3.up * y;
-            v[s + 1] = v1 + Vector3.up * y;
-            v[s + 2] = v2 + Vector3.up * y;
-            v[s + 3] = v3 + Vector3.up * y;
-            t[ti] = s + 1;
-            t[ti + 1] = s + 2;
-            t[ti + 2] = s;
-            t[ti + 3] = s + 3;
-            t[ti + 4] = s + 2;
-            t[ti + 5] = s + 1;
         }
     }
 }
