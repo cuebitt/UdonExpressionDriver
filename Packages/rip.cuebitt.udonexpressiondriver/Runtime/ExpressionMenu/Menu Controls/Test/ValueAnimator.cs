1eabfa4d6399175884b5c0ff84f5ce1c15e8fc4c
cuebitt <136763482+cuebitt@users.noreply.github.com>
2025-11-05 11:52:31 -0800

    Add ValueAnimator
    ValueAnimator changes the values of the three Puppet controls to demonstrate their functionality

---
+++ Packages/rip.cuebitt.udonexpressiondriver/Runtime/ExpressionMenu/Menu Controls/Test/ValueAnimator.cs
@@ -0,0 +1,36 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace UdonExpressionDriver
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ValueAnimator : UdonSharpBehaviour
+    {
+        [Header("Configuration")]
+        [SerializeField, Range(0f, 10f)] private float speed = 1f;
+        [SerializeField] private bool animateValues = true;
+    
+        [Header("Internal")]
+        [SerializeField] private RadialPuppet radialPuppet;
+        [SerializeField] private AxisPuppet twoAxisPuppet;
+        [SerializeField] private AxisPuppet fourAxisPuppet;
+    
+        public void Update()
+        {
+            if (!animateValues) return;
+        
+            var t = Mathf.PingPong(Time.time * speed, 1f);
+            var newValue =  Mathf.SmoothStep(0f, 1f, t);
+        
+            if(radialPuppet != null) radialPuppet.Value = newValue;
+            if (twoAxisPuppet != null)
+            {
+                twoAxisPuppet.PuppetValue = new Vector2(newValue, 1 - newValue);
+            }
+            if (fourAxisPuppet != null) 
+            {
+                fourAxisPuppet.PuppetValue = new Vector2(newValue, newValue);
+            }
+        }
+    }
+}
