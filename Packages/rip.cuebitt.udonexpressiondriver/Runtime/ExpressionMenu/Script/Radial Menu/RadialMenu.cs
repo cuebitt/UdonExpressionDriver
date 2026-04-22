57908ffa9ca2fd3d99c296349fede5fccf19b24e
Cuebitt <136763482+cuebitt@users.noreply.github.com>
2026-04-21 17:40:06 -0700

    Remove outline mesh generation
    Will re-add later

--- Packages/rip.cuebitt.udonexpressiondriver/Runtime/ExpressionMenu/Script/Radial Menu/RadialMenu.cs
+++ Packages/rip.cuebitt.udonexpressiondriver/Runtime/ExpressionMenu/Script/Radial Menu/RadialMenu.cs
@@ -18,8 +18,7 @@ namespace UdonExpressionDriver
         private const float DefaultInnerRadius = 0.1f;
         private const float DefaultOuterRadius = 0.3f;
         private const int DefaultRadialSteps = 48;
-        private const float DefaultOutlineThickness = 0.01f;
-        private const float DefaultLabelHeightOffset = 0.001f;
+        private const float DefaultLabelHeightOffset = 0.01f;
         private const float DefaultLabelScale = 0.25f;
         private const float DefaultLabelZOffset = 0.2f;
         private const int MaxSegmentArraySize = 8;
@@ -29,8 +28,8 @@ namespace UdonExpressionDriver
         [SerializeField] private float innerRadius = DefaultInnerRadius;
         [SerializeField] private float outerRadius = DefaultOuterRadius;
         [SerializeField] private int radialSteps = DefaultRadialSteps;
-        [SerializeField] private float outlineThickness = DefaultOutlineThickness;
-
+        [SerializeField] private float labelOffset = DefaultLabelHeightOffset;
+        
         [Header("Content")]
         [Tooltip("Text labels for each segment. Leave empty to hide the label.")]
         [SerializeField] private string[] labels;
@@ -90,16 +89,24 @@ namespace UdonExpressionDriver
                 if (meshHolder == null) continue;
 
                 meshHolder.localRotation = Quaternion.Euler(0f, angleStep * i - startAngle, 0f);
+                var pos = meshHolder.localPosition;
+                pos.y = 0f;
+                meshHolder.localPosition = pos;
 
                 var mf = meshHolder.GetComponent<MeshFilter>();
+                Mesh colliderMesh = null;
                 if (mf != null)
-                    mf.sharedMesh = CreateWedgeMesh(angleStep, innerRadius, outerRadius, radialSteps, outlineThickness);
+                {
+                    colliderMesh = CreateWedgeMesh(angleStep, innerRadius, outerRadius, radialSteps);
+                    mf.sharedMesh = colliderMesh;
+                }
+                    
 
                 var mr = meshHolder.GetComponent<MeshRenderer>();
                 if (mr != null) mr.sharedMaterials = new[] { gradientMaterial };
 
                 var mc = meshHolder.GetComponent<MeshCollider>();
-                if (mc != null && mf != null) mc.sharedMesh = mf.sharedMesh;
+                if (mc != null && mf != null && colliderMesh != null) mc.sharedMesh = colliderMesh;
             }
         }
 
@@ -162,7 +169,7 @@ namespace UdonExpressionDriver
 
                 label.localPosition = new Vector3(
                     Mathf.Sin(midAngle) * midRadius,
-                    DefaultLabelHeightOffset,
+                    labelOffset,
                     Mathf.Cos(midAngle) * midRadius - DefaultLabelZOffset * midRadius
                 );
 
@@ -179,9 +186,9 @@ namespace UdonExpressionDriver
         /// <param name="outerR">Outer radius of the wedge.</param>
         /// <param name="steps">Number of subdivisions along the arc.</param>
         /// <param name="outlineThickness">Width of the outline along the edges in world units.</param>
+        /// <param name="_outlineHeightOffset">Height of the outline mesh (prevents z-fighting)</param>
         /// <returns>A Mesh containing the wedge and its radial outline.</returns>
-        private static Mesh CreateWedgeMesh(float angleDeg, float innerR, float outerR, int steps,
-            float outlineThickness)
+        private static Mesh CreateWedgeMesh(float angleDeg, float innerR, float outerR, int steps)
         {
             // --- Base wedge ---
             var wedgeMesh = new Mesh();
@@ -225,92 +232,7 @@ namespace UdonExpressionDriver
             wedgeMesh.RecalculateNormals();
             wedgeMesh.RecalculateBounds();
 
-            // --- Generate radial outline ---
-            var outlineMesh = CreateOutlineMesh(angleDeg, innerR, outerR, outlineThickness);
-
-            // --- Merge wedge and outline ---
-            return MergeMeshes(wedgeMesh, outlineMesh);
-        }
-
-        /// <summary>
-        /// Generates a mesh containing two radial outline quads along the left and right edges of a wedge.
-        /// The quads have constant thickness along the wedge edges, are slightly raised along Y, fully inside the wedge,
-        /// and all UVs are set to (0,0). Normals point outward from the center.
-        /// </summary>
-        /// <param name="angleDeg">Angular span of the wedge in degrees.</param>
-        /// <param name="innerR">Inner radius of the wedge.</param>
-        /// <param name="outerR">Outer radius of the wedge.</param>
-        /// <param name="outlineThickness">Width of the outline in world units (radial thickness along the edge).</param>
-        /// <returns>A Mesh containing the left and right outline quads.</returns>
-        private static Mesh CreateOutlineMesh(float angleDeg, float innerR, float outerR, float outlineThickness)
-        {
-            var mesh = new Mesh();
-
-            var verts = new Vector3[8];
-            var uvs = new Vector2[8];
-            var tris = new int[12];
-
-            const float outlineY = DefaultLabelHeightOffset;
-            var angleRad = Mathf.Deg2Rad * angleDeg;
-
-            // Left edge outline (start of wedge)
-            SetOutlineStrip(0, 0f, innerR, outerR, outlineThickness, outlineY, verts, uvs, tris);
-
-            // Right edge outline (end of wedge)
-            SetOutlineStrip(4, angleRad, innerR, outerR, outlineThickness, outlineY, verts, uvs, tris);
-
-            mesh.vertices = verts;
-            mesh.uv = uvs;
-            mesh.triangles = tris;
-            mesh.RecalculateNormals();
-            mesh.RecalculateBounds();
-
-            return mesh;
-        }
-
-
-        /// <summary>
-        /// Generates a single radial outline quad along a wedge edge with constant thickness.
-        /// </summary>
-        /// <param name="index">Start index in the vertex array (0 or 4).</param>
-        /// <param name="baseAngle">Angle of the wedge edge (radians).</param>
-        /// <param name="innerR">Inner radius of the wedge.</param>
-        /// <param name="outerR">Outer radius of the wedge.</param>
-        /// <param name="outlineThickness">Width of the outline in world units along the edge.</param>
-        /// <param name="outlineY">Small Y offset to raise the outline above the wedge.</param>
-        /// <param name="verts">Vertex array to populate.</param>
-        /// <param name="uvs">UV array (all zero).</param>
-        /// <param name="tris">Triangle array to populate (two triangles forming the quad).</param>
-        private static void SetOutlineStrip(int index, float baseAngle, float innerR, float outerR,
-            float outlineThickness, float outlineY, Vector3[] verts, Vector2[] uvs, int[] tris)
-        {
-            // Direction along the wedge edge
-            var dir = new Vector3(Mathf.Sin(baseAngle), 0f, Mathf.Cos(baseAngle));
-
-            // Perpendicular direction in XZ plane for width of outline
-            var perp = new Vector3(dir.z, 0f, dir.x);
-
-            var offset = perp * (outlineThickness * 0.5f);
-
-            // Four vertices forming the quad (radial strip)
-            verts[index + 0] = dir * innerR - offset + Vector3.up * outlineY; // inner left
-            verts[index + 1] = dir * innerR + offset + Vector3.up * outlineY; // inner right
-            verts[index + 2] = dir * outerR - offset + Vector3.up * outlineY; // outer left
-            verts[index + 3] = dir * outerR + offset + Vector3.up * outlineY; // outer right
-
-            // All UVs zero
-            for (var i = 0; i < 4; i++) uvs[index + i] = Vector2.zero;
-
-            var tBase = (index / 4) * 6;
-
-            // Swap triangles to ensure outward-facing normals (clockwise from above)
-            tris[tBase + 0] = index + 0;
-            tris[tBase + 1] = index + 2;
-            tris[tBase + 2] = index + 1;
-
-            tris[tBase + 3] = index + 1;
-            tris[tBase + 4] = index + 2;
-            tris[tBase + 5] = index + 3;
+            return wedgeMesh;
         }
 
         /// <summary>
@@ -322,29 +244,19 @@ namespace UdonExpressionDriver
         /// <returns>A new Mesh combining both input meshes.</returns>
         private static Mesh MergeMeshes(Mesh a, Mesh b)
         {
-            var verts = new Vector3[a.vertexCount + b.vertexCount];
-            var uvs = new Vector2[a.vertexCount + b.vertexCount];
-            var tris = new int[a.triangles.Length + b.triangles.Length];
+            var combine = new CombineInstance[2];
+            
+            combine[0].mesh = a;
+            combine[1].mesh = b;
 
-            // Copy mesh A
-            for (var i = 0; i < a.vertexCount; i++) verts[i] = a.vertices[i];
-            for (var i = 0; i < a.vertexCount; i++) uvs[i] = a.uv[i];
-            for (var i = 0; i < a.triangles.Length; i++) tris[i] = a.triangles[i];
+            var m = new Mesh();
+            m.CombineMeshes(combine);
+            
+            m.RecalculateNormals();
+            m.RecalculateBounds();
 
-            // Copy mesh B
-            for (var i = 0; i < b.vertexCount; i++) verts[a.vertexCount + i] = b.vertices[i];
-            for (var i = 0; i < b.vertexCount; i++) uvs[a.vertexCount + i] = b.uv[i];
-            for (var i = 0; i < b.triangles.Length; i++) tris[a.triangles.Length + i] = b.triangles[i] + a.vertexCount;
-
-            // ReSharper disable once UseObjectOrCollectionInitializer
-            var mesh = new Mesh();
-            mesh.vertices = verts;
-            mesh.uv = uvs;
-            mesh.triangles = tris;
-            mesh.RecalculateNormals();
-            mesh.RecalculateBounds();
-
-            return mesh;
+            return m;
         }
+        
     }
 }
